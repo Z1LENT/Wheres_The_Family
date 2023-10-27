5b2e71e6bbf86ed4c3193bd9ef908350cc3d07fc
stortrut <35192303+stortrut@users.noreply.github.com>
2023-10-27 02:16:57 +0200

    main menu

--- Assets/SoundMainMenu.cs
+++ Assets/SoundMainMenu.cs
@@ -7,12 +7,17 @@ public class SoundMainMenu : MonoBehaviour
 
 
     public AudioSource ad;
+    public AudioSource zombieAd;
     public AudioClip lightning;
+    public AudioClip zombie;
 
     public void PlayLightning()
     {
         ad.PlayOneShot(lightning, 0.5f);
     }
-
+    public void PlayZombie()
+    {
+        zombieAd.PlayOneShot(zombie, 0.5f);
+    }
 
 }
